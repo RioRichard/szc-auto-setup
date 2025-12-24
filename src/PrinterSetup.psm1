d2dac3cb46e2654d9a088024c2b3da09833267a2
RioRichard <thanh.huuphungak47@gmail.com>
2025-12-24 10:23:17 +0700

    del: remove old code to move to python

--- src/PrinterSetup.psm1
+++
@@ -1,8 +0,0 @@
-function Invoke-PrinterSetup {
-    Write-Host "Running Printer Setup..."
-    # Placeholder for printer setup logic
-    Start-Sleep -Seconds 2
-    Write-Host "Printer Setup Complete."
-}
-
-Export-ModuleMember -Function Invoke-PrinterSetup
